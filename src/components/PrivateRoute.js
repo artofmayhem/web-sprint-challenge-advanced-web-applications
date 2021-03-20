1bd3f4e94c779e55eb5e0a4cb3c1c6c228d534ed
artofmayhem <chef_tony_miller@yahoo.com>
2021-03-19 14:27:30 -1000

    initial commit set up privateroute and AWA files

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,20 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render={(props) => {
+      if (localStorage.getItem("authToken")) {
+        return <Component {...props} />;
+      }
+      else { 
+        alert("You must login first")
+        return <Redirect to="/login" />;
+      }
+    }}
+    />
+  )
+}
 
-
-
-
-
-
-
-//Task List:
-//1. Build a PrivateRoute component that redirects if user is not logged in
+export default PrivateRoute;
